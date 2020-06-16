20d0d3b2a9000666d647a83a1dcadf5bfc42fd9e
K1M4K-ID <46388169+K1M4K-ID@users.noreply.github.com>
2020-06-16 10:50:57 +0700

    Update msfkit.sh

--- msfkit.sh
+++ msfkit.sh
@@ -238,7 +238,7 @@ echo $cy"             K1M4K-23035.portmap.io            "
 sleep 1
 echo
 echo
-echo $bi"     setiap ip berbeda beda, sesuaikan ip anda"
+echo $bi"    setiap ip berbeda beda, sesuaikan ip anda"
 echo "----------------------------------------------------";
 echo $hi
 read -p "masukan configs/ip lokal : " l
